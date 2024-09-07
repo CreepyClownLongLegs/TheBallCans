5d14cae88ac1cf54966b97af956b2973756f595b
kakdudl <jloncar0@gmail.com>
2024-09-07 19:56:27 +0200

    #5 Added Items and Inventory (P1)

---
+++ Assets/Scripts/InventoryPanel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Search;
+using UnityEngine;
+
+public class InventoryPanel : MonoBehaviour
+{
+    [SerializeField] ItemContainer inventory;
+
+    private void Start()
+    {
+        Show();
+    }
+
+    private void Show()
+    {
+        
+    }
+}
