5e641cc6295af609e166686b1b0cef1a0bde00b4
kakdudl <jloncar0@gmail.com>
2024-09-23 22:46:28 +0200

    added coin sounds

---
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class FMODEvents : MonoBehaviour
+{
+    [field: Header("Item SFX")]
+    [field: SerializeField] public EventReference itemCollected {get; private set;} 
+    public static FMODEvents instance {get; private set;}
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than once FMOD Events instance in the scene.");
+        }
+        instance = this;
+    }
+}
