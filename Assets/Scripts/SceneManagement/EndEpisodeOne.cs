38465569330bb80ebc63cf19c2f75a74646c8b76
CreepyClownLongLegs <e11927096@student.tuwien.ac.at>
2024-12-03 04:00:20 +0100

    #74 Added Ending Panels

--- Assets/Scripts/SceneManagement/EndEpisodeOne.cs
+++ Assets/Scripts/SceneManagement/EndEpisodeOne.cs
@@ -1,26 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndEpisodeOne : MonoBehaviour
 {
     [SerializeField]
     private GameObject endOfEpisodeOnePanel;
     private BoxCollider2D boxCollider2D;
+    private TextMeshProUGUI text;
+    string sleepingText = "zzZZzzZZ <br> zzZZZZzz";
+    string EpisodeTwoText = "Begin Of Episode Two: <br> You ain't so bad kid";
+    public float sleepingTimer = 30f;
+    public float fadeInOutTime = 50f;
+    Color black = Color.black;
+
 
     void Start()
     {   
         endOfEpisodeOnePanel.SetActive(false);
-        boxCollider2D = GetComponent<BoxCollider2D>();
-        boxCollider2D.enabled = EpisodeManager.instance.EpisodeOneRhytmGameFinished;
+    }
+
+    void OnEnabled(){
+
     }
 
     private void OnTriggerEnter2D(Collider2D collider2D){
-        if(collider2D.gameObject.CompareTag("Player")){
+        if(collider2D.gameObject.CompareTag("Player") && EpisodeManager.instance.EpisodeOneRhytmGameFinished && !EpisodeManager.instance.secondEpisode){
             endOfEpisodeOnePanel.SetActive(true);
             EpisodeManager.instance.firstEpisode = false;
             EpisodeManager.instance.secondEpisode = true;
             EpisodeManager.instance.ChangeDoorValue("Romania", true);
+            GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController2D>().elevatorPanelIsOpen = true;
+            boxCollider2D = GetComponent<BoxCollider2D>();
+            boxCollider2D.enabled = false;
+            //endOfEpisodeOnePanel.GetComponent<EndOfEPisodeOnePanel>().FadeInOut();
         }
     }
+
+
+
 }
---
+++ Assets/Scripts/SceneManagement/EndOfEPisodeOnePanel.cs
@@ -0,0 +1,73 @@
+using System.Threading;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndOfEPisodeOnePanel : MonoBehaviour
+{
+    private GameObject endOfEpisodeOnePanel;
+    private TextMeshProUGUI text;
+    string sleepingText = "zzZZzzZZ <br> zzZZZZzz";
+    string EpisodeTwoText = "Begin Of Episode Two: <br> You ain't so bad kid";
+    public float sleepingTimer = 3f;
+    public float fadeInOutTime = 5f;
+    Color black = Color.black;
+    public float timer = 0f;
+    public int frequinceOfText = 5;
+    bool isAddingRichTextTag = false;
+    int i = 0;
+    int j=0;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        endOfEpisodeOnePanel = this.gameObject;
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    void FixedUpdate(){
+        timer += 0.01f;
+        if(timer > sleepingTimer && timer < (sleepingTimer + 0.5f)){
+            ChangeColor();
+        } 
+        if(timer > (sleepingTimer + 0.5f) && timer < (sleepingTimer + 3f)){
+            showSleepingText();
+        }
+        if(timer > (sleepingTimer + 3f) && timer < (sleepingTimer + fadeInOutTime)){
+            text.text = EpisodeTwoText;
+        }
+        if(timer > (sleepingTimer + fadeInOutTime)){
+            GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController2D>().elevatorPanelIsOpen = false;
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void ChangeColor(){
+        text.text = "";
+        endOfEpisodeOnePanel.GetComponent<Image>().color = new Color(
+                Color.black.r,
+                Color.black.g,
+                Color.black.b,
+                Mathf.Lerp(0.2f, 1f, (timer-sleepingTimer)/ 0.3f)
+        );
+    }
+
+    public void showSleepingText(){
+        if(i >= frequinceOfText && j < sleepingText.Length){
+            if (sleepingText[j] == '<' || isAddingRichTextTag) 
+            {
+            isAddingRichTextTag = true;
+            if (sleepingText[j] == '>')
+                {
+                    isAddingRichTextTag = false;
+                }
+            }else {
+                text.text += sleepingText[j]; 
+            }
+            j++;
+            i = 0;
+        }
+        i++;
+    }
+
+}
