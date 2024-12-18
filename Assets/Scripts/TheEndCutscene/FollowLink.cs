5535dd1eb5d9a60d3720f00f22ee1fb2dd36d226
kakdudl <jloncar0@gmail.com>
2024-12-18 01:49:25 +0100

    98.25 changed link for questionnaire

--- Assets/Scripts/TheEndCutscene/FollowLink.cs
+++ Assets/Scripts/TheEndCutscene/FollowLink.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class FollowLink : MonoBehaviour
 {
     public void OpenLink(){
-        Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSemyn0CAvNttQu4peHn0dFKAreiB0MQqULC0CCk75Td1Ak4OQ/viewform?usp=header");
+        Application.OpenURL("https://forms.gle/vLmyRU4HNHfKA2rx5");
     }
 }
